519363bbf3144c64d1010f46897afe3e7c1f20aa
Lucas Mace <lucasmace4130@gmail.com>
2021-11-15 12:28:54 -0600

    Image Slider Logic Started

--- public/app.js
+++ public/app.js
@@ -23,3 +23,85 @@ const countdown = setInterval(() => {
         ${targetDateTime.minutes % 60} Minutes`;
     }
 }, 1000);
+
+const imageGallery = [ 
+    {
+        title: "Cycling Deals",
+        src: "Cycling_Slider.jpg",
+        link: "https://www.eriksbikeshop.com/cycling/bicycles",
+        target: "_self",
+        titlePosition: "TR"
+    },
+    {
+        title: "Ski Deals",
+        src: "Ski_Slider.jpg",
+        link: "https://www.eriksbikeshop.com/winter/ski",
+        target: "_self",
+        titlePosition: "BL"
+    },
+    {
+        title: "Snowboard Deals",
+        src: "Snowboard_Slider.jpg",
+        link: "https://www.eriksbikeshop.com/winter/snowboard",
+        target: "_self",
+        titlePosition: "BR"
+    },
+];
+
+const sliderImage = document.getElementById("slider-image");
+const sliderTitle = document.getElementById("slider-title");
+const sliderLink = document.getElementById("slider-link");
+const sliderImagePrefix = "./images/";
+let sliderIndex = 0;
+
+const slider = () => {
+    sliderImage.src = sliderImagePrefix + imageGallery[sliderIndex].src;
+    sliderTitle.innerHTML = imageGallery[sliderIndex].title;
+
+    switch (imageGallery[sliderIndex].titlePosition) {
+        case "TR":
+            sliderTitle.style.top = "2rem";
+            sliderTitle.style.right = "2rem";
+            sliderTitle.style.bottom = "auto";
+            sliderTitle.style.left = "auto";
+            
+            break;
+            case "TL":
+                sliderTitle.style.top = "2rem";
+                sliderTitle.style.right = "auto";
+                sliderTitle.style.bottom = "auto";
+                sliderTitle.style.left = "2rem";
+                
+            break;
+            case "BR":
+                sliderTitle.style.top = "auto";
+                sliderTitle.style.right = "2rem";
+                sliderTitle.style.bottom = "3.5rem";
+                sliderTitle.style.left = "auto";
+                
+            break;
+            case "BL":
+                sliderTitle.style.top = "auto";
+                sliderTitle.style.right = "auto";
+                sliderTitle.style.bottom = "3.5rem";
+                sliderTitle.style.left = "2rem";
+                
+            break;
+    
+        default:
+            break;
+    }
+
+    // sliderTitle.style.top = 
+
+    if (sliderIndex === imageGallery.length - 1) {
+        sliderIndex = 0;
+    } else {
+        sliderIndex++;
+    }
+}
+
+const sliderSeconds = 1;
+const sliderTimer = setInterval(() => {
+    slider();
+}, sliderSeconds * 1000);
